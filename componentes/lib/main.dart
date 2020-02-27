537d0b51728984ba606cfa6855aa477b13443b01
Jorge Santana <jorgesanure@gmail.com>
2020-02-27 17:45:09 -0500

    Loading external data and Using FutureBuilder

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:componentes/src/pages/home_temp.dart';
+import 'package:componentes/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
  
 void main() => runApp(MyApp());
@@ -8,12 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: HomePage(),
-      ),
+      home: HomePage(),
     );
   }
 }
---
+++ componentes/lib/src/pages/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import 'package:componentes/src/providers/user_provider.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Componentes")
+      ),
+      body: _getLista(),
+    );
+  }
+
+  Widget _getLista() => FutureBuilder(
+    future: userProvider.getData(),
+    initialData: [],
+    builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) 
+      => ListView(
+        children: _getListItems(snapshot.data),
+      )
+  );
+
+  List<Widget> _getListItems(List<dynamic> items) {
+    final List<Widget> _items = [];
+    items.forEach((item) {
+      _items.add(
+        ListTile(
+          title: Text(item['name']),
+          subtitle: Text(item['email']),
+          leading: Icon(Icons.account_circle),
+          trailing: Icon(Icons.keyboard_arrow_right),
+        )
+      );
+      _items.add(Divider());
+    });
+    return _items;
+  }
+}
