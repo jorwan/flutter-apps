04edea3de932431aff39f8ce276642be2338c5c7
Jorge Santana <jorgesanure@gmail.com>
2020-02-28 13:28:18 -0500

    Creating a Card

--- componentes/lib/src/pages/card_page.dart
+++ componentes/lib/src/pages/card_page.dart
@@ -7,5 +7,37 @@ class CardPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Cards')
       ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[
+          _getCard1()
+        ]
+      ),
+    );
+
+  Widget _getCard1()
+    => Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text("The New Way to Build Mobile App"),
+            subtitle: Text("Now you can lkjasd fasldk fjlaksjd flajs dlfkjas lkdjf lkasjd flkjas ldkfj alksdj flkasj ldkfjalksdjflkas df"),
+            leading: Icon(Icons.info),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              FlatButton(
+                child: Text("Read"),
+                onPressed: (){},
+              ),
+              FlatButton(
+                child: Icon(Icons.delete),
+                onPressed: (){},
+              )
+            ],
+          )
+        ],
+      )
     );
 }
