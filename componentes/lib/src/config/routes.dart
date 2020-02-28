3e27c1d20fb61a7a9dad00a8003ca239287e9e1e
Jorge Santana <jorgesanure@gmail.com>
2020-02-28 13:08:34 -0500

    Adding Card Page

--- componentes/lib/src/config/routes.dart
+++ componentes/lib/src/config/routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/pages/avatar_page.dart';
+import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/home_page.dart';
 
 // Set initial route to call on run app
@@ -11,7 +12,8 @@ final String initialRoute = '/';
 final Map<String, Widget Function(BuildContext context)> _routes = {
   '/'       : (BuildContext context) => HomePage(),
   'alert'   : (BuildContext context) => AlertPage(),
-  'avatar'  : (BuildContext context) => AvatarPage()
+  'avatar'  : (BuildContext context) => AvatarPage(),
+  'card'    : (BuildContext context) => CardPage()
 };
 
 class Router {
---
+++ componentes/lib/src/pages/card_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context)
+    => Scaffold(
+      appBar: AppBar(
+        title: Text('Cards')
+      ),
+    );
+}
