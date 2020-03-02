dc5e6cfe604a35480ae1280d631c1258bcdc9162
Jorge Santana <jorgesanure@gmail.com>
2020-03-02 10:17:25 -0500

    Adding circle avatar in AppBar

--- componentes/lib/src/pages/avatar_page.dart
+++ componentes/lib/src/pages/avatar_page.dart
@@ -5,7 +5,20 @@ class AvatarPage extends StatelessWidget {
   Widget build(BuildContext context)
     => Scaffold(
       appBar: AppBar(
-        title: Text('Avatar')
+        title: Text('Avatar'),
+        actions: <Widget>[
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 5.0, 5.0, 5.0),
+            child: ClipOval(
+              child: FadeInImage(
+                fit: BoxFit.cover,
+                width: 45.0,
+                image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Stan_Lee_by_Gage_Skidmore_3.jpg/800px-Stan_Lee_by_Gage_Skidmore_3.jpg'),
+                placeholder: AssetImage('images/loading.gif'),
+              ),
+            )
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.keyboard_arrow_left),
