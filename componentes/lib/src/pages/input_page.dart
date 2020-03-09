69f0f36511634445046fb20c70f629d2e2f465e5
Jorge Santana <jorgesanure@gmail.com>
2020-03-09 15:41:28 -0400

    Using Email & Password fields

--- componentes/lib/src/pages/input_page.dart
+++ componentes/lib/src/pages/input_page.dart
@@ -7,47 +7,78 @@ class InputPage extends StatefulWidget {
 
 class _InputPageState extends State<InputPage> {
   String _nombre = "";
-  
+  String _email = "";
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Input"),
-      ),
-      body: ListView(
-        padding: EdgeInsets.symmetric(
-          horizontal: 10.0,
+        appBar: AppBar(
+          title: Text("Input"),
         ),
-        children: <Widget>[
-          SizedBox(height: 25,),
-          TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30)
-              ),
-              contentPadding: EdgeInsets.only(left: 20),
-              labelText: "Name",
-              hintText: "Name of User",
-              hintStyle: TextStyle(
-                color: Color.fromRGBO(0, 0, 0, 0.1)
-              ),
-              helperText: "Ex. Jorge",
-              suffixIcon: Icon(Icons.person),
-              icon: Icon(Icons.account_circle),
-              counterText: "${_nombre.length}",
+        body: ListView(
+            padding: EdgeInsets.symmetric(
+              horizontal: 10.0,
             ),
-            onChanged: (text) {
-              setState(() {
-                _nombre = text;
-              });
-            },
-          ),
-          ListTile(
-            title: Text("Nombre: $_nombre"),
-          )
-        ]
-      )
-    );
+            children: <Widget>[
+              SizedBox(
+                height: 25,
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  contentPadding: EdgeInsets.only(left: 20),
+                  labelText: "Name",
+                  hintText: "Name of User",
+                  hintStyle: TextStyle(color: Color.fromRGBO(0, 0, 0, 0.1)),
+                  helperText: "Ex. Jorge",
+                  suffixIcon: Icon(Icons.person),
+                  icon: Icon(Icons.account_circle),
+                  counterText: "${_nombre.length}",
+                ),
+                onChanged: (text) {
+                  setState(() {
+                    _nombre = text;
+                  });
+                },
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30)),
+                    labelText: "Email",
+                    contentPadding: EdgeInsets.only(left: 20),
+                    icon: Icon(Icons.mail),
+                    suffixIcon: Icon(Icons.mail_outline)),
+                onChanged: (text) => setState(() {
+                  _email = text;
+                }),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  contentPadding: EdgeInsets.only(left: 20),
+                  labelText: "Password",
+                  suffixIcon: Icon(Icons.lock_outline),
+                  icon: Icon(Icons.lock),
+                  counterText: "${_nombre.length}",
+                ),
+              ),
+              ListTile(
+                title: Text("Nombre: $_nombre"),
+              ),
+              ListTile(
+                title: Text("Email: $_email"),
+              )
+            ]));
   }
 }
